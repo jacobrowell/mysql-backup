9ec9e823944399d2830a7f33a242c88ebeb4a3dc
jacobrowell <jacobrowell000@gmail.com>
2019-11-27 18:19:53 +0200

    add mysql host & port to settings

--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+MYSQL_HOST = os.getenv("MYSQL_HOST", 'root')
+MYSQL_PORT = os.getenv("MYSQL_PORT", 'root')
 MYSQL_USER = os.getenv("MYSQL_USER", 'root')
 MYSQL_PASS = os.getenv("MYSQL_PASS", 'root')
 BACKUP_PATH = os.getenv("BACKUP_PATH", '.')
