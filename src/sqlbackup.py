a9c399e3dcbd042e6759e7826171660b57e62730
jacobrowell <jacobrowell000@gmail.com>
2019-11-27 18:24:08 +0200

    update sqlbackup.py to use settings and sqlalchemy

--- src/sqlbackup.py
+++ src/sqlbackup.py
@@ -1,23 +1,35 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python3
 import os
 import subprocess
-from config import config
+from datetime import datetime
+
+from sqlalchemy import create_engine
+
+import settings
 
 
-user = config.get('user')
-password = config.get('password')
-backup_path = config.get('backup_dir')
+DB_BLACKLIST = ['mysql', 'phpmyadmin', 'information_schema', 'performance_schema']
+
+engine = create_engine("mysql+pymysql://{}:{}@{}:{}/?charset=utf8mb4".format(
+    settings.MYSQL_USER,
+    settings.MYSQL_PASS,
+    settings.MYSQL_HOST,
+    settings.MYSQL_PORT,
+))
+
+export_timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+user = settings.MYSQL_USER
+password = settings.MYSQL_PASS
+backup_path = settings.BACKUP_PATH
 
 if not os.path.exists(backup_path) or not os.path.isdir(backup_path):
     os.mkdir(backup_path, 0o755)
 
-databasesRaw = subprocess.check_output("mysql -u{} -p{} -e 'show databases;'".format(user, password), shell=True)
+databases = engine.execute("SHOW DATABASES").fetchall()
+databases = [db[0] for db in databases if db[0] not in DB_BLACKLIST]
 
-databases = databasesRaw.split()
-exclude_db = ['Database', 'mysql', 'phpmyadmin', 'information_schema', 'performance_schema']
-databases = [db for db in databases if db not in exclude_db]
-
-for dbName in databases:
-    command = 'mysqldump -u{} -p{} {} > {}/{}.sql &'.format(user, password, dbName, backup_path, dbName)
+for db_name in databases:
+    export_file_name = f"{db_name}_{export_timestamp}.sql"
+    command = f"mysqldump -u{user} -p{password} {db_name} > {backup_path}/{db_name}.sql &"
     subprocess.call(command, shell=True)
