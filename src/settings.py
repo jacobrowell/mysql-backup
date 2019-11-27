8f232e5c5f25c9a9d2771f5bdd8c1a82a7e163c3
jacobrowell <jacobrowell000@gmail.com>
2019-11-27 18:31:36 +0200

    add s3 config to settings

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,5 @@
 import os
+from distutils.util import strtobool
 from dotenv import load_dotenv
 
 
@@ -8,4 +9,9 @@ MYSQL_HOST = os.getenv("MYSQL_HOST", 'root')
 MYSQL_PORT = os.getenv("MYSQL_PORT", 'root')
 MYSQL_USER = os.getenv("MYSQL_USER", 'root')
 MYSQL_PASS = os.getenv("MYSQL_PASS", 'root')
+
 BACKUP_PATH = os.getenv("BACKUP_PATH", '.')
+
+S3_ENABLED = strtobool(os.getenv("S3_ENABLED", 'False'))
+S3_BUCKET = os.getenv("S3_BUCKET", '')
+S3_PATH = os.getenv("S3_PATH", '')
